acff9ade1f896bba233d983b1c800a4a37d95059
shark27 <44381346+jslzr27@users.noreply.github.com>
2019-04-10 00:10:09 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Clicky-Game
-
-Check the app here: https://jslzr27.github.io/Clicky-Game/.
---
+++ precache-manifest.8a3801c56cef1564cce53105d3117967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "1625588280dacf5ab9bf",
+    "url": "/Clicky-Game/static/js/main.884a2ddf.chunk.js"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/Clicky-Game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "1625588280dacf5ab9bf",
+    "url": "/Clicky-Game/static/css/main.e18e9be0.chunk.css"
+  },
+  {
+    "revision": "08ff7b904f43d473326a158e9bade58e",
+    "url": "/Clicky-Game/index.html"
+  }
+];
